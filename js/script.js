b067f5a37bc23c6a0e175631549219f9db1bddbb
FernandoVerderame <fernandoverderame96@hotmail.com>
2023-12-18 17:13:02 +0100

    added user choices validation

--- js/script.js
+++ js/script.js
@@ -16,34 +16,45 @@ console.log(userKilometres);
 const userAge = prompt('Qual è la tua età?');
 console.log(userAge);
 
+// Controllo scelta
+let errorMessage;
+
+if (isNaN(userKilometres) || isNaN(userAge) || userKilometres < 1 || userAge < 1)
+errorMessage = 'Devi inserire un numero valido in entrambi i campi!';
+
 // ! 5 Calcolare il prezzo totale del viaggio
 // 5 - A Calcolare il prezzo del biglietto (0.21 € al km)
-let totalPrice = userKilometres * 0.21;
-console.log(totalPrice);
+if (errorMessage) {
+    alert(errorMessage);
+} else {
 
-// 5 - B & C Sconto del 20% per i minorenni
-let discount20 = totalPrice * 20 / 100;
-let discount40 = totalPrice * 40 / 100;
-console.log(discount20, discount40);
+    let totalPrice = userKilometres * 0.21;
+    console.log(totalPrice);
 
-let discountPrice;
+    // 5 - B & C Sconto del 20% per i minorenni
+    let discount20 = totalPrice * 20 / 100;
+    let discount40 = totalPrice * 40 / 100;
+    console.log(discount20, discount40);
 
-if (userAge < 18) {
-    totalPrice += - discount20;
+    let discountPrice;
+
+    if (userAge < 18) {
+        totalPrice += - discount20;
+    }
+
+    if (userAge > 65) {
+        totalPrice += - discount40;
+    }
+
+    console.log(totalPrice);
+
+    // ! 6 Prezzo finale (con massimo due decimali)
+    totalPrice = Math.round((totalPrice + Number.EPSILON) * 100) / 100;
+    console.log(totalPrice);
+
+    // ! 7 Inseriamo il risultato nell'elemento HTML
+    // Aggiorniamo il messaggio
+    message += `<strong>${totalPrice} €</strong>`;
+
+    paragraph.innerHTML = message;
 }
-
-if (userAge > 65) {
-    totalPrice += - discount40;
-}
-
-console.log(totalPrice);
-
-// ! 6 Prezzo finale (con massimo due decimali)
-totalPrice = Math.round((totalPrice + Number.EPSILON) * 100) / 100;
-console.log(totalPrice);
-
-// ! 7 Inseriamo il risultato nell'elemento HTML
-// Aggiorniamo il messaggio
-message += `<strong>${totalPrice} €</strong>`;
-
-paragraph.innerHTML = message;
