8d71ef84984510ae52656ade4cd93e2c862682da
FernandoVerderame <fernandoverderame96@hotmail.com>
2023-12-18 15:40:01 +0100

    built message

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ console.log('JS OK');
 // ! 1 Prendo l'elemento dal DOM (la p)
 const paragraph = document.getElementById('result');
 console.log(paragraph);
+
+// ! 2 Costruiamo il messaggio
+let message = 'Il prezzo totale del viaggio è di: ';
+console.log(message);
