f8a163175542b414b6dedf2c9d4ceae830995000
FernandoVerderame <fernandoverderame96@hotmail.com>
2023-12-18 17:14:45 +0100

    refactoring javascript file

--- js/script.js
+++ js/script.js
@@ -1,20 +1,14 @@
-console.log('JS OK');
-
 // ! 1 Prendo l'elemento dal DOM (la p)
 const paragraph = document.getElementById('result');
-console.log(paragraph);
 
 // ! 2 Costruiamo il messaggio
 let message = 'Il prezzo totale del viaggio è di: ';
-console.log(message);
 
 // ! 3 Chiedere all'utente il numero di chilometri da percorrere
 const userKilometres = prompt('Quanti chilometri vuoi percorrere?');
-console.log(userKilometres);
 
 // ! 4 Chiedere all'utente l'età
 const userAge = prompt('Qual è la tua età?');
-console.log(userAge);
 
 // Controllo scelta
 let errorMessage;
@@ -29,12 +23,10 @@ if (errorMessage) {
 } else {
 
     let totalPrice = userKilometres * 0.21;
-    console.log(totalPrice);
 
     // 5 - B & C Sconto del 20% per i minorenni
     let discount20 = totalPrice * 20 / 100;
     let discount40 = totalPrice * 40 / 100;
-    console.log(discount20, discount40);
 
     let discountPrice;
 
@@ -46,11 +38,8 @@ if (errorMessage) {
         totalPrice += - discount40;
     }
 
-    console.log(totalPrice);
-
     // ! 6 Prezzo finale (con massimo due decimali)
     totalPrice = Math.round((totalPrice + Number.EPSILON) * 100) / 100;
-    console.log(totalPrice);
 
     // ! 7 Inseriamo il risultato nell'elemento HTML
     // Aggiorniamo il messaggio
